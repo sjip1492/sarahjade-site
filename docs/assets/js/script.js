836152eed3f5f2cf7ea28256a9655c2a65d444c9
Sarah Pratt <sjip1492@Sarahs-MacBook-Pro-372.local>
2026-01-20 01:06:21 -0800

    favicon and signature icon styling

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,6 +1,6 @@
 function loadContent(section) {
   // Check if this is a local HTML section (partial) or a full page
-  if (['bio', 'news', 'cv', 'work', 'headshot'].includes(section)) {
+  if (['bio', 'news', 'cv', 'work', 'work_index', 'news_index'].includes(section)) {
     fetch(`/partials/${section}.html`)
       .then(response => {
         if (!response.ok) throw new Error('Content not found');
@@ -18,8 +18,3 @@ function loadContent(section) {
     window.location.href = section;
   }
 }
-  // function loadHeadshot() {
-  //   document.getElementById('content').innerHTML = `
-  //     <img src="/assets/images/me2.png" alt="My Photo" class="img-fluid rounded">
-  //   `;
-  // }
